ec2baf8472c63c5294f321d97fdf90212a2c002d
Alban <dewilde@intechinfo.fr>
2019-03-13 09:41:58 +0100

    Play with the server and the javalin framework
    Try thing until it work
    Seriously, I try thing for a better understanding of the javalin framework

---
+++ server/src/main/java/serverFunctions.kt
@@ -0,0 +1,20 @@
+import io.javalin.Context
+
+fun accueil(ctx: Context) {
+}
+
+fun forbidden(ctx: Context) {
+    ctx.result("THIS IS FORBIDDEN !")
+}
+
+fun subscribeOrConnect(ctx: Context) {
+}
+
+fun pass_level(ctx: Context) {
+}
+
+fun give_more_logo(ctx: Context) {
+}
+
+fun how_the_fuck_i_play_your_game(ctx: Context) {
+}
