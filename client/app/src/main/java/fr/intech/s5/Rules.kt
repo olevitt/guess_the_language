09ab96296995af0be00cadfcfc8db4ef06b9193a
Unknown <acheng@intechinfo.fr>
2019-03-13 11:50:40 +0100

    Rules are now displayed in markdown

--- client/app/src/main/java/fr/intech/s5/Rules.kt
+++ client/app/src/main/java/fr/intech/s5/Rules.kt
@@ -2,19 +2,26 @@ package fr.intech.s5
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
-import kotlinx.android.synthetic.main.rules_activity.*
-import java.io.File
+import java.io.*
+import us.feras.mdv.MarkdownView
 
 class Rules: Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.rules_activity)
 
-        var rulesText : TextView = findViewById(R.id.rulesText)
-        var file : String = "./regles.md"
-        var rules : String = File(file).readText(Charsets.UTF_8)
+        var rulesText : MarkdownView = findViewById(R.id.rulesText)
+        val file : InputStream = resources.openRawResource(R.raw.regles)
+        var rules = ""
 
-        rulesText.text = rules
+        try {
+            rules = file.bufferedReader().use { it.readText() }
+            rulesText.loadMarkdown(rules)
+
+        } catch(e : Exception) {
+            e.printStackTrace()
+        }
     }
 }
